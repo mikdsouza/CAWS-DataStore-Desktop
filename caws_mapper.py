c4f63bec3a002f957dab436cd6491df32e831818
Mikhail D'Souza <mikdsouza34@gmail.com>
2015-12-15 18:29:31 +1300

    Got unique names of all columns

--- caws_mapper.py
+++ caws_mapper.py
@@ -1,4 +1,4 @@
-from openpyxl import Workbook, load_workbook
+from openpyxl import load_workbook
 
 from tkinter import *
 from tkinter import filedialog
@@ -14,8 +14,31 @@ def import_workbook(filename):
     wb = load_workbook(filename)
     w_sheets = wb.worksheets
 
+    cols = list_all_cols(w_sheets)
+    print(cols)
+
+
+def list_all_cols(w_sheets):
+    cols = []
+
     for ws in w_sheets:
-        print(w_sheets.name)
+        col_num = 1
+
+        while True:
+            if ws.cell(row=1, column=col_num).value is not None:
+                col_name = ws.cell(row=1, column=col_num).value
+            else:
+                col_name = ws.cell(row=2, column=col_num).value
+
+            if col_name is None or col_name == "":
+                break
+
+            if col_name not in cols:
+                cols.append(col_name)
+
+            col_num += 1
+
+    return cols
 
 
 def main():
